afc3b8a7ecf23052b08d3ef7473c7a8441493306
victoroh <seungwon.victor.oh@gmail.com>
2022-02-09 15:52:21 +0900

    remove duplicate code and split long method

--- Rental.java
+++ Rental.java
@@ -48,21 +48,27 @@ public class Rental {
 
 	public int getDaysRentedLimit() {
 		int limit = 0 ;
-		int daysRented ;
-		if (getStatus() == 1) { // returned Video
-			long diff = returnDate.getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		} else { // not yet returned
-			long diff = new Date().getTime() - rentDate.getTime();
-			daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
-		}
-		if ( daysRented <= 2) return limit ;
+		
+		if ( getRentedDays() <= 2) return limit ;
 
+		// switch
 		switch ( video.getVideoType() ) {
 			case Video.VHS: limit = 5 ; break ;
 			case Video.CD: limit = 3 ; break ;
 			case Video.DVD: limit = 2 ; break ;
 		}
 		return limit ;
+	}
+
+	public int getRentedDays() {
+		int daysRented;
+		long diff;
+		if (getStatus() == 1) {
+			diff = returnDate.getTime() - rentDate.getTime();			
+		} else {
+			diff = new Date().getTime() - rentDate.getTime();			
+		}
+		daysRented = (int) (diff / (1000 * 60 * 60 * 24)) + 1;
+		return daysRented;
 	}
 }
